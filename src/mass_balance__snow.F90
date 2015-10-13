5bd1cb4a41dd2288aef31cf53f9cadfb12d189b6
Steve Westenbroek <smwesten@usgs.gov>
2015-10-13 13:22:19 -0500

    Adding *.F90 files back to project.

---
+++ src/mass_balance__snow.F90
@@ -0,0 +1,36 @@
+module mass_balance__snow
+
+  use iso_c_binding, only        : c_short, c_int, c_float, c_double
+  implicit none
+
+contains
+ 
+  elemental subroutine calculate_snow_mass_balance( snow_storage,         &
+                                                    potential_snowmelt,   &
+                                                    snowmelt,             &
+                                                    snowfall )
+
+    real (kind=c_float), intent(inout)      :: snow_storage
+    real (kind=c_float), intent(inout)      :: snowmelt
+    real (kind=c_float), intent(in)         :: potential_snowmelt
+    real (kind=c_float), intent(in)         :: snowfall    
+
+
+    snow_storage = snow_storage + snowfall
+
+     if( snow_storage > potential_snowmelt ) then
+
+        snowmelt = potential_snowmelt
+
+      else   ! not enough snowcover to satisfy the amount that *could* melt
+        
+        snowmelt = snow_storage
+      
+      end if
+
+    snow_storage = max( snow_storage - snowmelt, 0.0_c_float )
+
+
+  end subroutine calculate_snow_mass_balance
+
+end module mass_balance__snow
---
+++ src/mass_balance__soil.F90
@@ -0,0 +1,50 @@
+module mass_balance__soil
+
+  use iso_c_binding, only        : c_float
+  implicit none
+
+  private
+
+  public :: calculate_soil_mass_balance
+
+contains
+
+   elemental subroutine calculate_soil_mass_balance( potential_recharge,    &
+                                                     soil_storage,          &
+                                                     soil_storage_max,      &
+                                                     actual_et,             &
+                                                     infiltration )
+
+    real (kind=c_float), intent(inout)      :: potential_recharge
+    real (kind=c_float), intent(inout)      :: soil_storage
+    real (kind=c_float), intent(inout)      :: actual_et
+    real (kind=c_float), intent(in)         :: soil_storage_max
+    real (kind=c_float), intent(in)         :: infiltration
+
+ 
+    ! [ LOCALS ]
+    real (kind=c_float) :: new_soil_storage
+
+    new_soil_storage = soil_storage + infiltration - actual_et
+
+    if ( new_soil_storage < 0.0_c_float ) then
+
+      actual_et = max( 0.0_c_float, soil_storage + infiltration )
+      soil_storage = 0.0_c_float
+      potential_recharge = 0.0_c_float
+
+    elseif ( new_soil_storage > soil_storage_max ) then
+
+      potential_recharge = new_soil_storage - soil_storage_max
+      soil_storage = soil_storage_max
+
+    else
+
+      soil_storage = new_soil_storage
+      potential_recharge = 0.0_c_float
+
+    endif
+
+  end subroutine calculate_soil_mass_balance
+
+end module mass_balance__soil
