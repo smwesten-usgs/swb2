35e1869ce85ebbf8ed1b18d2cf5dd44ad0be4492
Steve Westenbroek <smwesten@usgs.gov>
2017-10-23 15:46:14 -0500

    Update CMake files for Fedora.

--- build/linux_x64/gfortran/run_cmake_gfortran.sh
+++ build/linux_x64/gfortran/run_cmake_gfortran.sh
@@ -1,56 +1,64 @@
-#!/bin/sh
+#!/bin/bash
 #remove existing Cmake cache and directories
 # remove existing Cmake cache and directories
 rm -fr CMake*
+rm -rf Testing
+rm -rf src
+rm -rf tests
+rm -f CPack*
 rm -f *.txt
 
+export GCC_VERSION=6.4.1
+export GCC_TRIPLET=x86_64-redhat-linux
 # set CMAKE-related and build-related variables
-export CMAKEROOT=/usr/bin
-export GCC_VERSION=5.3.1
-export COMPILER_TRIPLET=x86_64-redhat-linux
-export Fortran_COMPILER_NAME=gfortran
+export GCCLIST=$( locate gcc-6 | grep bin | grep $GCC_VERSION )
+export GCCARR=($GCCLIST)
+export GCC=${GCCARR[1]}
+export GFORTRANLIST=$( locate gfortran-6 | grep bin | grep $GCC_VERSION )
+export GFORTRANARR=($GFORTRANLIST)
+export GFORTRAN=${GFORTRANARR[1]}
+
+export CMAKEROOT=/usr/bin/cmake
 export R_HOME=/usr/bin/R
+
+export LIB_HDF5_HL=$( locate hdf5_hl.a | grep /usr/local/lib )
+export LIB_HDF5=$( locate hdf5.a | grep /usr/local/lib )
+export LIB_NETCDF=$( locate netcdf.a | grep /usr/local/lib )
+export LIB_Z=$( locate libz.a | grep /usr/lib64 )
+#export LIB_SZ=$( locate libsz.a | grep /usr/lib64 )
+export LIB_DL=/usr/lib64/libdl.so
+export LIB_GCC=$( locate libgcc.a | grep $GCC_TRIPLET | grep -v 32 )
+export LIB_GFORTRAN=$( locate libgfortran.a | grep $GCC_TRIPLET | grep -v 32 )
+
+export PATH=/usr/local:/usr/local/bin:/usr/local/lib:/usr/bin/cmake:$PATH
 
 # define where 'make copy' will place executables
 export INSTALL_PREFIX=/usr/local/bin
 
 # define other variables for use in the CMakeList.txt file
 # options are "Release" or "Debug"
-export BUILD_TYPE="Release"
-# options are "x86" (32-bit) or "x64" (64-bit)
-export PLATFORM_TYPE="x64"
+export BUILD_TYPE="DEBUG"
+export SYSTEM_TYPE="MacOS"
 
 # define platform and compiler specific compilation flags
-export CMAKE_Fortran_FLAGS_DEBUG="-O0 -g -ggdb -fbacktrace -fcheck=all -fexceptions -ffree-line-length-none -finit-local-zero"
-#set CMAKE_Fortran_FLAGS_RELEASE="-O2 -mtune=native -floop-parallelize-all -flto -ffree-line-length-none -static-libgcc -static-libgfortran"
-export CMAKE_Fortran_FLAGS_RELEASE="-O2 -mtune=native -g -fbacktrace -ffree-line-length-none -finit-local-zero"
+export CMAKE_Fortran_FLAGS_DEBUG="-O0 -g -ggdb -Wuninitialized -fbacktrace -fcheck=all -fexceptions -fmax-errors=6 -fbackslash -ffree-line-length-none -Wno-maybe-uninitialized"
+export CMAKE_Fortran_FLAGS_RELEASE="-O2 -mtune=native -ffree-line-length-512 -fbackslash -ffpe-summary='none' -Wno-maybe-uninitialized"
 
 # set important environment variables
-export FC=/usr/bin/gfortran
-export CC=/usr/bin/gcc
-export CXX=/usr/bin/g++
-export AR=/usr/bin/gcc-ar
-export NM=/usr/bin/gcc-nm
+export FC=$GFORTRAN
+export CC=$GCC
+export CXX=$GPP
 
-set LIBRARY_PATH=/usr/lib
-
-# set compiler-specific link and compile flags
-export LDFLAGS="-flto -ldl"
-export CPPFLAGS="-DgFortran"
-
-/usr/bin/cmake ../../.. -G "Unix Makefiles" \
--DGCC_VERSION="$GCC_VERSION " \
--DCOMPILER_TRIPLET="$COMPILER_TRIPLET " \
--DFortran_COMPILER_NAME="$Fortran_COMPILER_NAME " \
--DFC="$FC "                       \
--DCC="$CC "                       \
--DLIBGCC_PATH="/usr/lib/gcc/x86_64-redhat-linux/5.3.1/"  \
--DPLATFORM_TYPE="$PLATFORM_TYPE " \
--DBUILD_TYPE="$BUILD_TYPE " \
--DCMAKE_INSTALL_PREFIX:PATH="$INSTALL_PREFIX " \
--DLIB_PATH1="/usr/lib64" \
--DLIB_PATH2="/usr/local/lib" \
--DLIB_PATH3="/usr/local/lib64" \
--DCMAKE_Fortran_FLAGS_DEBUG="$CMAKE_Fortran_FLAGS_DEBUG " \
+cmake "../../.." -G "Unix Makefiles"                         \
+-DSYSTEM_TYPE="$SYSTEM_TYPE "                                \
+-DCMAKE_BUILD_TYPE="$BUILD_TYPE "                            \
+-DLIB_HDF5_HL="$LIB_HDF5_HL "                                \
+-DLIB_HDF5="$LIB_HDF5 "                                      \
+-DLIB_Z="$LIB_Z "                                            \
+-DLIB_DL="$LIB_DL "                                          \
+-DLIB_NETCDF="$LIB_NETCDF "                                  \
+-DLIB_GCC="$LIB_GCC "                                        \
+-DLIB_GFORTRAN="$LIB_GFORTRAN "                              \
+-DCMAKE_INSTALL_PREFIX:PATH="$INSTALL_PREFIX "               \
+-DCMAKE_Fortran_FLAGS_DEBUG="$CMAKE_Fortran_FLAGS_DEBUG "    \
 -DCMAKE_Fortran_FLAGS_RELEASE="$CMAKE_Fortran_FLAGS_RELEASE"
-
