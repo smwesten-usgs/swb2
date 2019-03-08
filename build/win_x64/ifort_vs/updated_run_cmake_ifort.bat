3cdbdab8994d3e942b32df353f85ad748ee5a78f
Steve Westenbroek <smwesten@usgs.gov>
2019-03-08 17:08:00 -0600

    Add batch files that set CMake variables for ifort compilation to repo

--- build/win_x64/ifort_vs/updated_run_cmake_ifort.bat
+++ build/win_x64/ifort_vs/updated_run_cmake_ifort.bat
@@ -3,11 +3,9 @@
 del /F /Q CMakeCache.*
 rmdir /S /Q CMakeFiles
 rmdir /S /Q src
-rmdir /S /Q Win32
-rmdir /S /Q Debug
 rmdir /S /Q Testing
-rmdir /S /Q Release
 rmdir /S /Q tests
+rmdir /S /Q Win32
 rmdir /S /Q .vs
 del /S /Q *.txt
 del /S /Q *.sln
@@ -20,8 +18,9 @@ set Fortran_COMPILER_NAME=ifort
 set CMAKE_C_COMPILER=icl
 set NETCDF_VERSION='netCDF 4.6.1'
 
-set VERBOSE=0
-
+:: set path to Intel build tools
+set INTEL_LIBDIR="C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\compiler\lib\intel64_win"
+set INTEL_BINDIR="c:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\bin\intel64"
 set MAKE_EXECUTABLE_NAME=nmake
 
 :: explicitly locate each key library
@@ -30,11 +29,18 @@ for /f "delims=" %%x in ('where /R "c:\Program Files" libhdf5.lib') do call set 
 for /f "delims=" %%x in ('where /R "c:\Program Files" zlibstatic.lib') do call set LIB_Z=%%x
 for /f "delims=" %%x in ('where /R "c:\Program Files" netcdf.lib') do call set LIB_NETCDF=%%x
 
+::echo 1) %IFORT_EXE%
 :: substitute forward slash for backward slash
 set LIB_HDF5_HL=%LIB_HDF5_HL:\=/%
 set LIB_HDF5=%LIB_HDF5:\=/%
 set LIB_NETCDF=%LIB_NETCDF:\=/%
 set LIB_Z=%LIB_Z:\=/%
+::set IFORT_EXE=%IFORT_EXE:\=/%
+
+::set LIB_HDF5_HL=%LIB_HDF5_HL: =\ %
+::set LIB_HDF5=%LIB_HDF5: =\ %
+::set LIB_NETCDF=%LIB_NETCDF: =\ %
+::set LIB_Z=%LIB_Z: =\ %
 
 :: define where 'make copy' will place executables
 set INSTALL_PREFIX=d:\DOS
@@ -44,35 +50,31 @@ set INSTALL_PREFIX=d:\DOS
 set BUILD_TYPE="Debug"
 
 :: options are "x86" (32-bit) or "x64" (64-bit)
-set SYSTEM_TYPE="win_x64"
+set SYSTEM_TYPE="x64"
 
 :: define platform and compiler specific compilation flags
-set CMAKE_Fortran_FLAGS_DEBUG="/Od /fpp /MTd /debug:full /check:all /traceback"
-set CMAKE_Fortran_FLAGS_RELEASE="/O2 /QxHost /MT /fpp /traceback"
+set CMAKE_Fortran_FLAGS_DEBUG="/Od /fpp /MTd /Z7 /heap-arrays /debug:full /check:all /traceback /warn"
+set CMAKE_Fortran_FLAGS_RELEASE="/O2 /QxHost /MT /heap-arrays /F300000000 /fpp /traceback"
 set CMAKE_C_FLAGS_DEBUG="/Od /MTd /debug:full"
 set CMAKE_C_FLAGS_RELEASE="/O2 /QxHost /MT"
-set CMAKE_GENERATOR_PLATFORM="x64"
-::set CMAKE_EXE_LINKER_FLAGS='/NODEFAULTLIB:"LIBCMT"'
-::set LINKER_FLAGS="/nodefaultlib:vcomp"
 
 set SWB_EXTERNAL_LIBS="%LIB_HDF5_HL%;%LIB_HDF5%;%LIB_NETCDF%;%LIB_Z%"
-set CMAKE_OUTPUT="NMake Makefiles"
+set CMAKE_OUTPUT="Visual Studio 15 2017"
 
 for %%i in (%CMAKE_OUTPUT%) do (
 
 echo "Running CMake for target %%i"
 cmake ..\..\.. -G %%i ^
--DCMAKE_Fortran_COMPILER=%Fortran_COMPILER_NAME%              ^
--DCMAKE_C_COMPILER=%CMAKE_C_COMPILER%                         ^
--DSWB_EXTERNAL_LIBS=%SWB_EXTERNAL_LIBS%                       ^
--DSYSTEM_TYPE=%SYSTEM_TYPE%                                   ^
--DCMAKE_BUILD_TYPE=%BUILD_TYPE%                               ^
--DCMAKE_INSTALL_PREFIX:PATH=%INSTALL_PREFIX%                  ^
--DCMAKE_Fortran_FLAGS_DEBUG=%CMAKE_Fortran_FLAGS_DEBUG%       ^
--DCMAKE_Fortran_FLAGS_RELEASE=%CMAKE_Fortran_FLAGS_RELEASE%   ^
--DCMAKE_EXE_LINKER_FLAGS=%CMAKE_EXE_LINKER_FLAGS%             ^
--DCMAKE_GENERATOR_PLATFORM=%CMAKE_GENERATOR_PLATFORM%         ^
--DCMAKE_C_FLAGS_DEBUG=%CMAKE_C_FLAGS_DEBUG%                   ^
+-DCMAKE_Fortran_COMPILER=%Fortran_COMPILER_NAME% ^
+-DCMAKE_C_COMPILER=%CMAKE_C_COMPILER%   ^
+-DSWB_EXTERNAL_LIBS=%SWB_EXTERNAL_LIBS% ^
+-DSYSTEM_TYPE=%SYSTEM_TYPE%             ^
+-DCMAKE_BUILD_TYPE=%BUILD_TYPE%         ^
+-DCMAKE_INSTALL_PREFIX:PATH=%INSTALL_PREFIX%                ^
+-DCMAKE_Fortran_FLAGS_DEBUG=%CMAKE_Fortran_FLAGS_DEBUG%     ^
+-DCMAKE_Fortran_FLAGS_RELEASE=%CMAKE_Fortran_FLAGS_RELEASE% ^
+-DCMAKE_EXE_LINKER_FLAGS=%CMAKE_EXE_LINKER_FLAGS% ^
+-DCMAKE_C_FLAGS_DEBUG=%CMAKE_C_FLAGS_DEBUG%       ^
 -DCMAKE_C_FLAGS_RELEASE=%CMAKE_C_FLAGS_RELEASE%
 
 )
