c62edbeb1ee8d8eaa12543b1e52b9f937afeb7dd
Steve Westenbroek <smwesten@usgs.gov>
2019-10-17 16:07:18 -0600

    CMake variable name needs to be ALL CAPS (NETCDF vs NetCDF)

--- cmake/FindNetCDF.cmake
+++ cmake/FindNetCDF.cmake
@@ -1,4 +1,4 @@
-find_library(NetCDF_LIBRARY
+find_library(NETCDF_LIBRARY
   NAMES netcdf.a netcdf netcdf11 libnetcdf11
   HINTS ${NETCDF_LIB_DIR} /usr "c:/MinGW64" "c:/Program Files"
   PATH_SUFFIXES lib/x86_64-linux-gnu/ local/lib/ local/lib64 x86_64-w64-mingw32/lib "NetCDF"
