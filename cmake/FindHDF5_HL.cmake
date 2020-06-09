fe331a829fa51b0d1e0f3627c56836bbf3fba571
Steve Westenbroek <smwesten@usgs.gov>
2020-06-09 11:45:54 -0600

    Convert CMake files to reference the ENV variable LD_LIBRARY_PATH instead of local CMake valiable of same name

--- cmake/FindHDF5_HL.cmake
+++ cmake/FindHDF5_HL.cmake
@@ -1,13 +1,12 @@
 find_library(HDF5_HL_LIBRARY
-  NAMES hdf5_hl.a hdf5_hl libhdf5_serial_hl
-  HINTS ${LD_LIBRARY_PATH} /usr "c:/MinGW64" "c:/Program Files"
-  PATH_SUFFIXES lib/x86_64-linux-gnu/ local/lib/ lib/x86_64-linux-gnu/hdf5/serial/  local/lib64 x86_64-w64-mingw32/lib "netCDF"
+  NAMES hdf5_hl libhdf5_hl libhdf5_serial_hl
+  PATHS ENV LD_LIBRARY_PATH /usr "c:/MinGW64" "c:/Program Files"
+  PATH_SUFFIXES  lib lib64 lib/x86_64-linux-gnu/ local/lib/ 
   DOC "hdf5_hl library")
 
 if(HDF5_HL_LIBRARY)
   add_library(HDF5_HL_LIBRARY UNKNOWN IMPORTED
               GLOBAL
   )
+  add_dependencies(HDF5_HL_LIBRARY HDF5_LIBRARY ZLIB_LIBRARY)
 endif()
-
-add_dependencies(HDF5_HL_LIBRARY HDF5_LIBRARY ZLIB_LIBRARY)
