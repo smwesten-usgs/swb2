f352e5f84f66a021d3fa99067474dfe188ffc10f
Steve Westenbroek <smwesten@usgs.gov>
2020-07-21 11:50:45 -0500

    build: modify CMake files to allow detection of HDF5 libs on Windows

--- cmake/FindHDF5_HL.cmake
+++ cmake/FindHDF5_HL.cmake
@@ -1,6 +1,6 @@
 find_library(HDF5_HL_LIBRARY
   NAMES hdf5_hl libhdf5_hl libhdf5_serial_hl
-  PATHS ENV LD_LIBRARY_PATH ENV HDF5_DIR /usr "c:/MinGW64" "c:/Program Files"
+  PATHS ENV LD_LIBRARY_PATH ENV HDF5_DIR /usr "d:/MinGW64" "c:/Program Files"
   PATH_SUFFIXES lib lib/x86_64-linux-gnu/ local/lib/ local/lib64 x86_64-w64-mingw32/lib lib/x86_64-linux-gnu/hdf5/serial/ "netCDF"
   DOC "hdf5_hl library")
 
