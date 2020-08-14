82796a6615fd1a08008fca6b2fd583fda96b90a3
Steve Westenbroek <smwesten@usgs.gov>
2020-08-14 11:21:01 -0600

    build: update Yeti build script

--- build/yeti/gfortran/run_cmake_gfortran.sh
+++ build/yeti/gfortran/run_cmake_gfortran.sh
@@ -11,9 +11,6 @@ rm -f *.txt
 export GFORTRAN=gfortran
 export GCC=gcc
 
-# define where 'make copy' will place executables
-export INSTALL_PREFIX=/usr/local/bin
-
 # define other variables for use in the CMakeList.txt file
 # options are "Release" or "Debug"
 export BUILD_TYPE="Release"
@@ -26,13 +23,17 @@ export CMAKE_Fortran_FLAGS_RELEASE="-O2 -mtune=native -ffree-line-length-512 -fb
 export FC=$GFORTRAN
 export CC=$GCC
 
-module purge && module load legacy && module load gcc/6.1 && \
-  module load hdf5-serial/1.10.1-gcc6.1.0 && \
-  module load netcdf-serial/4.4.1.1-hdf51.10.1-gcc6.1.0 && \
-  module load cmake/3.15.4 && \
-  module load zlib/1.2.11-gcc && \
+#module purge && module load legacy && module load gcc/6.1 && \
+#  module load hdf5-serial/1.10.1-gcc6.1.0 && \
+#  module load netcdf-serial/4.4.1.1-hdf51.10.1-gcc6.1.0 && \
+#  module load cmake/3.15.4 && \
+#  module load zlib/1.2.11-gcc && \
+
+#module swap phdf5 hdf5/1.10.5 &&                          \ 
+
+module load netcdf/4.7.1  &&                                 \
+  module load cmake &&                                        \
 cmake "../../.." -G "Unix Makefiles"                         \
 -DCMAKE_BUILD_TYPE="$BUILD_TYPE "                            \
--DCMAKE_INSTALL_PREFIX:PATH="$INSTALL_PREFIX "               \
 -DCMAKE_Fortran_FLAGS_DEBUG="$CMAKE_Fortran_FLAGS_DEBUG "    \
 -DCMAKE_Fortran_FLAGS_RELEASE="$CMAKE_Fortran_FLAGS_RELEASE"
