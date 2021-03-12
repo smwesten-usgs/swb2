fe9d4c842c81975ed9f3f0919e78515fd758ec45
Steve Westenbroek <smwesten@usgs.gov>
2021-03-12 12:00:39 -0600

    chore: cleanup conflicted files

--- test/unit_tests/test_exceptions__index_values_valid.F90
+++ test/unit_tests/test_exceptions__index_values_valid.F90
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-module test_exceptions
-
-  use exceptions
-  use fruit
-  use iso_c_binding, only :c_int, c_float, c_double, c_bool
-  implicit none
-
-contains
-
-!-------------------------------------------------------------------------------
-
-  subroutine test_check_array_bounds_1d
-
-    integer (c_int) :: integer_values(615)
-    real (c_float)  :: float_values(23)
-    real (c_double) :: double_values(42)
-
-    logical, parameter :: TRUE =.true.
-    logical, parameter :: FALSE =.false.
-
-    call assert_equals( TRUE, logical(index_values_valid(integer_values, 25)) )
-
-  end subroutine test_check_array_bounds_1d
-
-end module test_exceptions
-=======
 module test_exceptions
 
   use exceptions
@@ -52,4 +25,3 @@ contains
   end subroutine test_check_array_bounds_1d
 
 end module test_exceptions
->>>>>>> fao56_tweaks
