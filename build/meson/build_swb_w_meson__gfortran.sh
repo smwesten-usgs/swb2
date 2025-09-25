a878047c5dd30fceacd7cf9a80b8785224c7f042
Steve Westenbroek <smwesten@usgs.gov>
2025-09-25 11:48:18 -0500

    fix: add comma to format statement to eliminate gfortran 'Legacy Extension: Missing comma in FORMAT string at (1)' warning

--- build/meson/build_swb_w_meson__gfortran.sh
+++ build/meson/build_swb_w_meson__gfortran.sh
@@ -5,8 +5,8 @@ rm -rf builddir
 mkdir builddir
 export FC=gfortran && export CC=gcc && export CXX=g++ && meson setup builddir ../.. && cd builddir && meson compile
 cd src
-sudo cp swb2 /usr/local/bin/swb2
-sudo cp swbstats2 /usr/local/bin/swbstats2
+cp swb2 /d/bin/swb2
+cp swbstats2 /d/bin/swbstats2
 
 cd ../test/unit_tests
 cp swbtest ../../../../../test/unit_tests/swbtest
