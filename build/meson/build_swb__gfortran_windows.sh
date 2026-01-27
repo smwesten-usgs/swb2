cb54b46c2cd136fd04205eee13dfb85426add6da
Steve Westenbroek <smwesten@usgs.gov>
2026-01-27 12:58:16 -0600

    build: eliminate 'inc_dirs'; modify build script for gfortran-ucrt64-windows

--- build/meson/build_swb__gfortran_windows.sh
+++ build/meson/build_swb__gfortran_windows.sh
@@ -3,10 +3,13 @@
 
 rm -rf builddir
 mkdir builddir
-export FC=gfortran && export CC=gcc && export CXX=g++ && meson setup builddir ../.. && cd builddir && meson compile
+
+export LATEST_NETCDF=/ucrt64/bin
+
+export FC=gfortran && export CC=gcc && export CXX=g++ && meson setup -Dnetcdf_root=${LATEST_NETCDF} builddir ../.. && cd builddir && meson compile
 cd src
-cp swb2 /d/bin/swb2
-cp swbstats2 /d/bin/swbstats2
+cp swb2 /usr/local/bin/swb2
+cp swbstats2 /usr/local/bin/swbstats2
 
 cd ../test/unit_tests
 cp swbtest ../../../../../test/unit_tests/swbtest
