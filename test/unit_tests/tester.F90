785c78ebbd85e43fc8ce21680117cd7f4a6fa452
Steve Westenbroek <smwesten@usgs.gov>
2026-07-08 15:00:36 -0500

    test: add new tests of fstring_list module

--- test/unit_tests/tester.F90
+++ test/unit_tests/tester.F90
@@ -8,7 +8,8 @@ program tester
 
   use, intrinsic :: iso_fortran_env, only: error_unit
   use testdrive, only: run_testsuite, new_testsuite, testsuite_type, &
-                       select_suite, run_selected, get_argument
+                       select_suite, run_selected, get_argument, &
+                       unittest_type
   use test_timer, only: collect_timer
   use test_allocatable_string, only: collect_allocatable_string
   use test_exceptions, only: collect_exceptions
@@ -16,11 +17,13 @@ program tester
   use test_datetime, only: collect_datetime
   use test_fao56, only: collect_fao56
   use test_constants_and_conversions, only: collect_constants_and_conversions
+  use test_fstring_list, only: collect_fstring_list
   implicit none
 
-  integer :: stat, suite_index
+  integer :: stat, suite_index, total_tests
   character(len=:), allocatable :: suite_name, test_name
   type(testsuite_type), allocatable :: testsuites(:)
+  type(unittest_type), allocatable :: temp_suite(:)
   character(len=*), parameter :: fmt = '("#", *(1x, a))'
 
   ! ANSI color codes
@@ -38,7 +41,8 @@ program tester
     new_testsuite("gash", collect_gash), &
     new_testsuite("datetime", collect_datetime), &
     new_testsuite("fao56", collect_fao56), &
-    new_testsuite("constants", collect_constants_and_conversions) &
+    new_testsuite("constants", collect_constants_and_conversions), &
+    new_testsuite("fstring_list", collect_fstring_list) &
   ]
 
   call get_argument(1, suite_name)
@@ -71,11 +75,21 @@ program tester
     end do
   end if
 
+  ! Count total tests across all suites
+  total_tests = 0
+  do suite_index = 1, size(testsuites)
+    call testsuites(suite_index)%collect(temp_suite)
+    total_tests = total_tests + size(temp_suite)
+    deallocate(temp_suite)
+  end do
+
   if (stat > 0) then
-    write(error_unit, '(a, i0, 1x, a, a)') RED, stat, "test(s) failed!", RESET
+    write(error_unit, '(a, i0, " of ", i0, " test(s) in ", i0, " suite(s) failed!", a)') &
+      RED, stat, total_tests, size(testsuites), RESET
     error stop 1, quiet=.true.
   else
-    write(error_unit, '(a, a, a)') GREEN, "All tests passed.", RESET
+    write(error_unit, '(a, i0, " tests in ", i0, " suites: All passed.", a)') &
+      GREEN, total_tests, size(testsuites), RESET
   end if
 
 end program tester
